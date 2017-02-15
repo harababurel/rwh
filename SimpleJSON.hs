89a569948b9b3fcc0a0b563e0e3bc20c10942b4c
Sergiu Puscas <srg.pscs@gmail.com>
2017-02-15 16:22:41 +0200

    implemented basic JSON algebraic type

---
+++ SimpleJSON.hs
@@ -0,0 +1,47 @@
+module SimpleJSON
+    (
+      JValue(..) -- export the type + all of its constructors
+    , getString
+    , getInt
+    , getDouble
+    , getBool
+    , getArray
+    , getObject
+    , isNull
+    ) where
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JArray [JValue]
+            | JObject (String, JValue)
+              deriving (Eq, Ord, Show)
+
+
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _           = Nothing
+
+getInt :: JValue -> Maybe Int
+getInt (JNumber x) = Just (truncate x)
+getInt _           = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber x) = Just x
+getDouble _           = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool x) = Just x
+getBool _         = Nothing
+
+getArray :: JValue -> Maybe [JValue]
+getArray (JArray xs) = Just xs
+getArray _           = Nothing
+
+getObject :: JValue -> Maybe (String, JValue)
+getObject (JObject x) = Just x
+getObject _           = Nothing
+
+isNull :: JValue -> Bool
+isNull x = x == JNull
